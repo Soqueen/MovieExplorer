6bfd5c49db004dc19af07efccd0fa1e620afb9c7
Aimee Ascencio <aimee_16_94@hotmail.com>
2017-02-08 19:52:06 -0800

    Created models for Movie Ratings and Users

--- movie_explorer/movie_rating/admin.py
+++ movie_explorer/movie_rating/admin.py
@@ -2,5 +2,5 @@ from django.contrib import admin
 from .models import User, Rating
 
 # Register your models here.
-admin.site.register(User)
-admin.site.register(Rating)
+admin.site.register(UserME)
+admin.site.register(MovieRatings)
--- movie_explorer/movie_rating/models.py
+++ movie_explorer/movie_rating/models.py
@@ -1,12 +1,12 @@
-from django.db import models
+ffrom django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
-class User(models.Model):
-    username = models.CharField(max_length=40, unique=True)
-    password = models.CharField(max_length=15, default='secret')
 
-class Rating(models.Model):
+class MovieRatings(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    movie_name = models.CharField(max_length=200)
-    rating = models.FloatField(default=0)
-    
+    movie_id = models.IntegerField(null = False, default = -1)
+    rating = models.IntegerField(null = False, default  = 0)
+
+class UserME(User):
+    member_since = models.DateField()

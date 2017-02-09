7d620b331dcc252cb8b7ebc66e2107db6558f89f
Felix Dube <felix.dube@mail.mcgill.ca>
2017-02-09 10:22:21 -0500

    Add template views

--- movie_explorer/movie_explorer/urls.py
+++ movie_explorer/movie_explorer/urls.py
@@ -16,11 +16,11 @@ Including another URLconf
 from django.conf.urls import include, url
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
+from django.views.generic.base import TemplateView
 
 urlpatterns = [
     url(r'^', include('movie_rating.urls')),
     url(r'^admin/', admin.site.urls),
     url(r'^login/$', auth_views.login, name='login'),
     url(r'^logout/$', auth_views.logout, name='logout'),
-    url(r'^admin/', admin.site.urls),
 ]

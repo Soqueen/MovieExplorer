d9b14350d2b52512c74a294b06cbb7b85e2b3984
Felix Dube <felix.dube@mail.mcgill.ca>
2017-02-09 10:02:18 -0500

    Add routes for user loggin

--- movie_explorer/movie_explorer/urls.py
+++ movie_explorer/movie_explorer/urls.py
@@ -19,4 +19,7 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^', include('movie_rating.urls')),
     url(r'^admin/', admin.site.urls),
+    url(r'^login/$', auth_views.login, name='login'),
+    url(r'^logout/$', auth_views.logout, name='logout'),
+    url(r'^admin/', admin.site.urls),
 ]

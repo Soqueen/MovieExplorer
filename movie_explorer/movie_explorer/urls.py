bd853fc243b93ff2574acba6a0142b7e24c4a90b
kurohikari <arthur.geneau@gmail.com>
2017-02-11 18:42:00 -0500

    Revert "Add home page"
    This reverts commit 6d87b002c7811a24a72aa06ce808f8b912183a2b.

--- movie_explorer/movie_explorer/urls.py
+++ movie_explorer/movie_explorer/urls.py
@@ -21,7 +21,6 @@ from django.views.generic.base import TemplateView
 urlpatterns = [
     url(r'^', include('movie_rating.urls')),
     url(r'^admin/', admin.site.urls),
-    url(r'^home/$', TemplateView.as_view(template_name='home.html'), name='home'),
     url(r'^login/$', auth_views.login, name='login'),
     url(r'^logout/$', auth_views.logout, name='logout'),
 ]

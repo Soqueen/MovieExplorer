959b6239d343101abf33888968656707a5187a1f
ahtml <anandhtml@gmail.com>
2017-02-16 00:34:33 -0500

    Added test cases to determine if the API returned any movies.

--- movie_explorer/movie_rating/tests.py
+++ movie_explorer/movie_rating/tests.py
@@ -1,13 +1,20 @@
 from django.test import TestCase
-
+# from .models import MovieRatings, UserME
+from .views import MovieView
 # Create your tests here.
 
-from .models import MovieRatings, UserME
+
 
 class Story4Cases(TestCase):
+    def test_is_movie_List_Empty(self):
+        """
+        Test to see if there are any movies that are displayed.
+        If the dictionary returned was empty, then know that API failed
+        """
 
-	def number_of_movies_displayed(self):
-		"""
-		Test to see if only 10 movies are displayed
-		"""
-		
+        new_context = MovieView.get_context_data(self) # Return query of movies
+        len_context = len(new_context) # Check how many movies have been returned
+        moviesFound = False
+        if(len_context>0): # Check if there is any movie returned
+            moviesFound = True
+        self.assertIs(moviesFound,True)

31b2c6bf79f0460cf24f113c28ef602dff503c32
ahtml <anandhtml@gmail.com>
2017-02-15 19:55:51 -0500

    Started initial test.

--- movie_explorer/movie_rating/tests.py
+++ movie_explorer/movie_rating/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+from .models import MovieRatings, UserME
+
+class Story4Cases(TestCase):
+
+	def number_of_movies_displayed(self):
+		"""
+		Test to see if only 10 movies are displayed
+		"""
+		

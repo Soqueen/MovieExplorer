d5e2f74f46577a93ac83d5bbd3ba1951e20249d8
Soqueen <sokhenglim973@gmail.com>
2017-02-16 22:50:44 -0500

    remove UserMe table

--- movie_explorer/movie_rating/models.py
+++ movie_explorer/movie_rating/models.py
@@ -9,11 +9,3 @@ class MovieRatings(models.Model):
     movie_id = models.IntegerField(null=False, default=-1)
     rating = models.IntegerField(null=False, default=0)
 
-
-class UserME(models.Model):
-    member_since = models.DateField()
-    first_name = models.CharField(max_length=20, default='')
-    last_name = models.CharField(max_length=20, default='')
-    email = models.EmailField(max_length=70, null=True, blank=True, unique=True)
-    username = models.CharField(unique=True, max_length=20, default='')
-    password = models.CharField(max_length=15, default='secret')

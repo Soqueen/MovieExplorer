49e5b8d273485554a971ef57f150d07f04da1706
ahtml <anandhtml@gmail.com>
2017-02-19 15:48:10 -0500

    Updated ST4-1 to ensure that only 10 movies are being displayed.

--- movie_explorer/movie_rating/tests.py
+++ movie_explorer/movie_rating/tests.py
@@ -7,25 +7,27 @@ from django.conf import settings
 
 
 class Story4Cases(TestCase):
-    def test_is_movie_List_Empty(self):
+    def test_ST4_1_is_movie_List_Empty(self):
         """
         Test to see if there are any movies that are displayed.
+        Check to see if there are 10 movies which are returned to display as per specs
         If the dictionary returned was empty, then know that API failed
         """
 
         new_context = MovieView.get_context_data(self) # Return query of movies
-        len_context = len(new_context) # Check how many movies have been returned
+        len_context = len(new_context['results']) # Check how many movies have been returned; found in results part of dictionary
         moviesFound = False
-        if(len_context>0): # Check if there is any movie returned
+        # print(len_context)
+        if(len_context == 10): # Check if there are 10 movies returned
             moviesFound = True
         self.assertIs(moviesFound,True)
 
+    """
     def test_is_API_missing_error(self):
-        """
-        Test to see if the appropriate error is return if the API is missing.
-        """
-        #hold_key = settings.TMDB_API_KEY
-        settings.TMDB_API_KEY = ''
+
+# Test to see if the appropriate error is return if the API is missing.
+
         new_content = MovieView.get_context_data(self)
         print(new_content)
         print(settings.TMDB_API_KEY)
+    """

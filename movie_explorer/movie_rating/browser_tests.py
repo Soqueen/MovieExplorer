1f10e82762d2ec3542ac3baccd83cd6af606153c
Soqueen <sokhenglim973@gmail.com>
2017-03-01 22:16:48 -0500

    add st5 test2 for sorting ascending order

--- movie_explorer/movie_rating/browser_tests.py
+++ movie_explorer/movie_rating/browser_tests.py
@@ -23,6 +23,11 @@ LOCAL_URL = 'http://127.0.0.1:8000/'
 URL = 'http://movieexplorer.ddns.net/'
 WAIT_TIME = 3  # wait time for browser to stay open for 3 seconds
 
+# ST5-Sorting Feature
+# TODO the tag may change
+SORT_BOX_TAG = 'sortOption'
+SORT_OPTION_TAG = 'option'
+
 
 class ChromeTest(unittest.TestCase):
     # Anything declared in setUp will be executed for all test cases
@@ -36,25 +41,22 @@ class ChromeTest(unittest.TestCase):
 
     # An individual test case. Must start with 'test_' (as per unittest module)
     def test_home_page(self):
-        driver = self.driver
-        # Go to google.com
-        driver.get(self.base_url)
+        self.driver.get(self.base_url)
         # A test to ensure the page has keyword 'The Movie Explorer' in the page title
-        self.assertEqual('The Movie Explorer', driver.title)
+        self.assertEqual('The Movie Explorer', self.driver.title)
 
         # Pauses the screen for 5 seconds so we have time to confirm it arrived at the right page
         time.sleep(WAIT_TIME)
 
         # Take a screen shot of the results. Make sure to put your image test name.
-        self.take_screen_shot('test_home_page')
+        self.take_screen_shot('test_homepage')
 
     # test_ST4_1_is_movie_List_Empty
     def test_st1(self):
         """
-        Test for registration. Note, It is the left over from the first sprint, so we do not implement the all the test cases
+        Test for registration. Note, It is the left over from the first sprint, so we do not implement all the test cases
         :return:
         """
-        # Go to google.com
         self.driver.get(os.path.join(self.base_url, 'register'))
         # Pauses the screen for 5 seconds so we have time to confirm it arrived at the right page
         time.sleep(WAIT_TIME)
@@ -98,14 +100,13 @@ class ChromeTest(unittest.TestCase):
         time.sleep(WAIT_TIME)
 
         # Take a screen shot of the results
-        self.take_screen_shot('test_register')
+        self.take_screen_shot('test_st1')
 
     def test_st2(self):
         """
-        Test for login. Note, It is the left over from the first sprint, so we do not implement the all the test cases
+        Test for login. Note, It is the left over from the first sprint, so we do not implement  all the test cases
         :return:
         """
-        # Go to google.com
         self.driver.get(os.path.join(self.base_url, 'login'))
         # Pauses the screen for 5 seconds so we have time to confirm it arrived at the right page
         time.sleep(WAIT_TIME)
@@ -134,14 +135,36 @@ class ChromeTest(unittest.TestCase):
         time.sleep(WAIT_TIME)
 
         # Take a screen shot of the results
-        self.take_screen_shot('test_login')
+        self.take_screen_shot('test_st2')
 
     def test_st5_1(self):
         """
-        Test to Sort by release date acceding order
+        Test to Sort the movies by release date acceding order.
         :return: None
         """
-        
+        self.driver.get(self.base_url)
+        # Pauses the screen for 5 seconds so we have time to confirm it arrived at the right page
+        time.sleep(WAIT_TIME)
+
+        # Sort select option
+        try:
+            # TODO the find may change
+            sort_select = self.driver.find_element_by_name(SORT_BOX_TAG)
+            for option in sort_select.find_elements_by_tag_name(SORT_OPTION_TAG):
+                if option.text == 'Release Date Ascending':
+                    option.click()
+                    break
+        except NoSuchElementException:
+            raise Exception('Cannot find Element name')
+
+        # Make sure the results page returned something
+        assert "No results found." not in self.driver.page_source
+
+        # Another pause so we can see what's going on
+        time.sleep(WAIT_TIME)
+
+        # Take a screen shot of the results
+        self.take_screen_shot('test_st5_1')
 
     def take_screen_shot(self, test_name):
         """

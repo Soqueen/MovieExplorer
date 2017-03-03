c3d67d04f523816ee4789c36b025ed8378855bef
ahtml <anandhtml@gmail.com>
2017-03-03 16:03:47 -0500

    Added test cases for ST6. These stories were added by Sam from Anand's machine.

--- movie_explorer/movie_rating/browser_tests.py
+++ movie_explorer/movie_rating/browser_tests.py
@@ -349,6 +349,7 @@ class ChromeTest(unittest.TestCase):
         There should only be one movie displayed
         :return: None
         """
+        # TODO test case for keyword with 0 result
         self.driver.get(self.base_url)
         # Pauses the screen so we have time to confirm it arrived at the right page
         time.sleep(WAIT_TIME)
@@ -483,8 +484,8 @@ class ChromeTest(unittest.TestCase):
         # Sort select option
         try:
             # TODO the name of genre class may change
-            sort_select = self.driver.find_element_by_name(GENRE_BOX_TAG)
-            for option in sort_select.find_elements_by_tag_name(GENRE_OPTION_TAG):
+            sort_select = self.driver.find_element_by_name(FILTER_BOX_TAG)
+            for option in sort_select.find_elements_by_tag_name(FILTER_OPTION_TAG):
                 if option.text == 'Action':
                     option.click()
                     break
@@ -512,8 +513,8 @@ class ChromeTest(unittest.TestCase):
         # Sort select option
         try:
             # TODO the name of genre class may change
-            sort_select = self.driver.find_element_by_name(GENRE_BOX_TAG)
-            for option in sort_select.find_elements_by_tag_name(GENRE_OPTION_TAG):
+            sort_select = self.driver.find_element_by_name(FILTER_BOX_TAG)
+            for option in sort_select.find_elements_by_tag_name(FILTER_OPTION_TAG):
                 if option.text == 'Action':
                     option.click()
                     break
@@ -541,8 +542,8 @@ class ChromeTest(unittest.TestCase):
         # Sort select option
         try:
             # TODO the name of genre class may change
-            sort_select = self.driver.find_element_by_name(GENRE_BOX_TAG)
-            for option in sort_select.find_elements_by_tag_name(GENRE_OPTION_TAG):
+            sort_select = self.driver.find_element_by_name(FILTER_BOX_TAG)
+            for option in sort_select.find_elements_by_tag_name(FILTER_OPTION_TAG):
                 if option.text == 'Action':
                     option.click()
                     break
@@ -600,6 +601,7 @@ class ChromeTest(unittest.TestCase):
 
     def test_st6_5(self):
         """
+        This testcase is exactly the same as st 5_5
         Test to Sort the movies by release date descending order after filtering by genre.
         :return: None
         """
@@ -635,7 +637,7 @@ class ChromeTest(unittest.TestCase):
         time.sleep(WAIT_TIME)
 
         # Take a screen shot of the results
-        self.take_screen_shot('test_st5_5')
+        self.take_screen_shot('test_st6_5')
 
     def test_st6_6(self):
         """
@@ -651,30 +653,21 @@ class ChromeTest(unittest.TestCase):
             # TODO the find may change
             filter_select = self.driver.find_element_by_name(FILTER_BOX_TAG)
             for option in filter_select.find_elements_by_tag_name(FILTER_OPTION_TAG):
-                if option.text == 'Comedy':
+                if option.text == 'Mock1':
                     option.click()
                     break
-        except NoSuchElementException:
-            raise Exception('Cannot find Element name')
-        # Sort select option
-        try:
-            # TODO the find may change
-            sort_select = self.driver.find_element_by_name(SORT_BOX_TAG)
-            for option in sort_select.find_elements_by_tag_name(SORT_OPTION_TAG):
-                if option.text == 'Popularity':
-                    option.click()
-                    break
+            # TODO implement blank page verification method here
         except NoSuchElementException:
             raise Exception('Cannot find Element name')
 
-        # Make sure the results page returned something
-        assert "No results found." not in self.driver.page_source
+            # Make sure the results page returned something
+            assert "No results found." not in self.driver.page_source
 
-        # Another pause so we can see what's going on
-        time.sleep(WAIT_TIME)
+            # Another pause so we can see what's going on
+            time.sleep(WAIT_TIME)
 
-        # Take a screen shot of the results
-        self.take_screen_shot('test_st5_6')
+            # Take a screen shot of the results
+            self.take_screen_shot('test_st6_6')
 
     def take_screen_shot(self, test_name):
         """

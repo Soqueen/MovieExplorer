26c127739f1f0e855da91278d1fa4f6b8c2b8167
ahtml <anandhtml@gmail.com>
2017-03-03 10:58:07 -0500

    Added ST4_2 to search for keyword Batman.

--- movie_explorer/movie_rating/browser_tests.py
+++ movie_explorer/movie_rating/browser_tests.py
@@ -364,7 +364,7 @@ class ChromeTest(unittest.TestCase):
             search_button = self.driver.find_element_by_css_selector("input[type=\"submit\"]")
             search_button.click()
         except NoSuchElementException:
-            raise Exception('Canot find Element search button')
+            raise Exception('Cannot find Element search button')
 
         #Pauses the screen so we have time to confirm we have the right page
         time.sleep(WAIT_TIME)
@@ -378,7 +378,40 @@ class ChromeTest(unittest.TestCase):
         # Take a screen shot of the results
         self.take_screen_shot('test_st4_1')
 
+    def test_st4_2(self):
+        """
+        Display movies for search "Batman"
+        There should be multiple movies displayed
+        :return: None
+        """
+        self.driver.get(self.base_url)
+        # Pauses the screen so we have time to confirm it arrived at the right page
+        time.sleep(WAIT_TIME)
 
+        try:
+            search_area = self.driver.find_element_by_name('search')
+            search_area.clear()
+            search_area.sendKeys("Batman")
+        except NoSuchElementException:
+            raise Exception('Cannot find Element search')
+
+        try:
+            search_button = self.driver.find_element_by_css_selector("input[type=\"submit\"]")
+            search_button.click()
+        except NoSuchElementException:
+            raise Exception('Cannot find Element search button')
+
+        #Pauses the screen so we have time to confirm we have the right page
+        time.sleep(WAIT_TIME)
+
+        # Make sure the results page returned something
+        assert "No results found." not in self.driver.page_source
+
+        # Another pause so we can see what's going on
+        time.sleep(WAIT_TIME)
+
+        # Take a screen shot of the results
+        self.take_screen_shot('test_st4_1')
 
 
     def take_screen_shot(self, test_name):

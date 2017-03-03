442f1f774c87694450ae6c0681803ea353954eaf
joannahalpern <joanna.halpern@mail.mcgill.ca>
2017-03-03 18:18:19 -0500

    redirected most pages to new home page instead of movie (but can't figure out why redirct of login is still going to movies)

--- movie_explorer/movie_rating/urls.py
+++ movie_explorer/movie_rating/urls.py
@@ -5,10 +5,10 @@ from django.views.generic.base import TemplateView
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.sort, name='sort'),
+    url(r'^$', views.sort, name='home'),
     url(r'^register/', views.register, name='register'),
     url(r'^login/', auth_views.login, name='login'),
-    url(r'^logout/', auth_views.logout, {'next_page': '/movies/'}, name='logout'),
+    url(r'^logout/', auth_views.logout, {'next_page': 'home'}, name='logout'),
     url(r'^movies/', views.MovieView.as_view(), name='movies'),
     url(r'^search/', views.search, name='search'),
 ]

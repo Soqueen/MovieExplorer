306767f7f2d54e32b8c45d3321ad7b5cce0468bf
Aimee Ascencio <aimee_16_94@hotmail.com>
2017-03-21 19:58:51 -0700

    FIXED CHROME BUG, no more listeners FIX average star update messing up the display

--- movie_explorer/movie_rating/static/js/description.js
+++ movie_explorer/movie_rating/static/js/description.js
@@ -3,26 +3,20 @@
  * Created by aimee on 3/18/17.
  */
 
-$(document).ready(function(){
-    $(".star").change(function(event) {
-
-        var star_val = $(event.target).val();
-        var movie = $("input[name=id_movie]").val();
-        $.ajax({
-            method: 'POST',
-            data: {
-                csrfmiddlewaretoken: document.cookie.split('=')[1],
-                action: "rate_movie",
-                rating: star_val,
-                movie_id: movie,
-            }
-        });
-
-        $('#average_rate').load(' #average_rate')
-
+function rate_movie (){
+    var star_val = $("input[type='radio'][name='star']:checked").val();
+    var movie = $("input[name=id_movie]").val();
+    $.ajax({
+        method: 'POST',
+        data: {
+            csrfmiddlewaretoken: document.cookie.split('=')[1],
+            action: "rate_movie",
+            rating: star_val,
+            movie_id: movie,
+        }
     });
-
-});
+    $('#containerS3').load(' #containerS3');
+}
 
 
 // JQuery for Popup example

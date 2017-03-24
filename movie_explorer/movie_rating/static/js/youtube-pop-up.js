623d98d216f31b6e2928234bcd49404d143a7b65
Soqueen <sokhenglim973@gmail.com>
2017-03-24 09:57:47 -0400

    add popup video

---
+++ movie_explorer/movie_rating/static/js/youtube-pop-up.js
@@ -0,0 +1,48 @@
+$(document).ready(function() {
+    $(".youtube-pop-up").click(function(event){
+        event.preventDefault();
+
+        // If element DNE
+        if($('#youtube-pop-video').length == 0){
+            var div = document.createElement('div');    
+            div.setAttribute('id', 'youtube-pop-video');
+            document.body.append(div);
+        } 
+
+        $('#youtube-pop-video').css({
+            "background-color": "rgba(0, 0, 0, 0.5)",       
+            "position": "absolute",
+            "top": "0",            
+            "text-align": "center",                                    
+            "width": "100%",
+            "height": "120%",
+            "padding-top": "15%",
+            "z-index": "1100",
+        });
+        
+        var div = document.createElement("div");
+        div.setAttribute("style", "padding-right:200px;float:right;font-size:40px");
+
+        var x = document.createElement("i");
+        x.setAttribute("class", "fa fa-times");
+        x.setAttribute("aria-hidden", "true");
+        x.setAttribute("style", "color:rgba(250,250,250,0.8); cursor: pointer;");
+        
+        var iframe = document.createElement('iframe');
+        iframe.setAttribute('src', $(this).attr('href').replace("watch?v=", "embed/"));
+        iframe.setAttribute('width', "70%");
+        iframe.setAttribute('height', "600");
+        iframe.setAttribute('frameborder', "0");
+
+        div.appendChild(x);
+        $('#youtube-pop-video').append(div);  
+        $('#youtube-pop-video').append("<br>");                
+        $('#youtube-pop-video').append("<br>");                
+        $('#youtube-pop-video').append("<br>");                
+        $('#youtube-pop-video').append(iframe);
+
+        $(".fa-times").click(function(){
+            $('#youtube-pop-video').remove();
+        });
+    });
+});

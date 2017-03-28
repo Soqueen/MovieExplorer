33e71c50730be6273e6083c3744987bd910d1720
joannahalpern <joanna.halpern@mail.mcgill.ca>
2017-03-28 05:18:06 -0400

    finished rating for my ratings page

---
+++ movie_explorer/movie_rating/static/js/myratings.js
@@ -0,0 +1,45 @@
+//------For the stars---------
+function highlightStar(obj,id) {
+	removeHighlight(id);
+	$('#stars-new-'+id+' li').each(function(index) {
+		$(this).addClass('highlight');
+		if(index == $('#stars-new-'+id+' li').index(obj)) {
+			return false;
+		}
+	});
+}
+
+function removeHighlight(id) {
+	$('#stars-new-'+id+' li').removeClass('selected');
+	$('#stars-new-'+id+' li').removeClass('highlight');
+}
+
+function addRating(obj,id) {
+	$('#stars-new-'+id+' li').each(function(index) {
+		$(this).addClass('selected');
+		$('#stars-new-'+id+' #rating').val((index+1));
+		if(index == $('#stars-new-'+id+' li').index(obj)) {
+			return false;
+		}
+	});
+    $.ajax({
+        method: 'POST',
+        data: {
+            csrfmiddlewaretoken: document.cookie.split('=')[1],
+            action: "rate_movie",
+            rating: $('#stars-new-' + id + ' #rating').val(),
+            movie_id: id,
+        }
+    });
+}
+
+function resetRating(id) {
+	if($('#stars-new-'+id+' #rating').val() != 0) {
+		$('#stars-new-'+id+' li').each(function(index) {
+			$(this).addClass('selected');
+			if((index+1) == $('#stars-new-'+id+' #rating').val()) {
+				return false;
+			}
+		});
+	}
+}
